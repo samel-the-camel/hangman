0de0771141e2c0e14f5e70a77795611a8f2d66db
samel-the-camel <86019158+samel-the-camel@users.noreply.github.com>
2021-09-11 10:06:07 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,3 +1,3 @@
 All in my head I'm thinkin' dealing with these thoughts tryna focus on breathin!
-
+How you gon' give up? You not eeeeven tryyy!
 I can imagine no other girl with me, come get these diamonds and these pearls with meeeee!
